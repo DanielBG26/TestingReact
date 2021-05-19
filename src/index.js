638cd2a8378d2eec697eaa4d782e842fac9d7194
DanielBG26 <danielbbgg@hotmail.com>
2021-05-19 21:49:12 +0200

    TestingReact

---
+++ src/index.js
@@ -0,0 +1,28 @@
+// const element = document.createElement('h1');
+// element.innerText = 'Hello, Platzi Badges!';
+
+// const container = document.getElementById('app');
+
+// container.appendChild(element);
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.css';
+//import './global.css';
+
+import Badge from './components/Badge';
+
+
+const container = document.getElementById('app');
+
+/// ReactDOM.render(__qué__, __dónde__);
+ReactDOM.render(
+	<Badge 
+		firstName="Daniel" 
+		lastName="Bermúdez" 
+		avatar = "//gravatar.com/avatar/2007403d1fff04f37d16509ff0eaabd1?s=200"
+		jobTitle="Frontend Engineer" 
+		twitter="@DanielBermudez" 
+	/>, 
+	container
+);
